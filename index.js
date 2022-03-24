ace02fdea6afa1f41041aef1ef839ae061ad8157
Kanak <kanakyadav406@gmail.com>
2022-03-24 19:18:01 +0530

    Port issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require('./src/app.js')
-PORT = 5000 || process.env.PORT
+PORT = process.env.PORT || 5000  
 const server = http.createServer(app);
 
 server.listen(PORT,()=>{
