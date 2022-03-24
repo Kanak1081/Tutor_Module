4ae8ec68abcb681f6bfbc2541370188d96dda2be
Kanak <kanakyadav406@gmail.com>
2022-03-24 19:30:57 +0530

    Third commit

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ const server = http.createServer(app);
 server.listen(PORT,()=>{
     console.log(`Server is running at Port ${PORT}`)
 })
-app.use(express.static(path.join(__dirname, "./dist")))
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist', 'index.html'))
-})
